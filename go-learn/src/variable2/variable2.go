bee6f4dff2e978444886ee8a3307d01e876f8ae5
kureharyosuke <ohhayo.com@gmail.com>
2021-05-09 21:42:06 +0900

    1. Variable

---
+++ go-learn/src/variable2/variable2.go
@@ -0,0 +1,20 @@
+package variable2
+
+import "fmt"
+
+func main() {
+	//여러개 선언	
+	var (
+		name string = "machine"
+		height int32
+		weight float32
+		isRunning bool
+		// 가독성과, 그룹을 묶어 하나의 상태를 의미
+	)
+
+	height = 250
+	weight = 350.56
+	isRunning = true
+
+	fmt.Println("name : ", name, "height : ", height, "weight : ", weight, "isRunning : ", isRunning)
+}
